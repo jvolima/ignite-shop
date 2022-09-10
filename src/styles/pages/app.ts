f781dc1284bb1b4de6c43fbeac323e7a194d60d8
João Vitor Lima <jvolima2004@gmail.com>
2022-09-10 11:11:22 -0300

    Estrutura da Home

--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -11,6 +11,6 @@ export const Container = styled("div", {
 export const Header = styled("header", {
   padding: '2rem 0',
   width: '100%',
-  maxWidth: 1100,
+  maxWidth: 1180,
   margin: '0 auto'
 })
